19b2ff65a91c28efeee62fd73c1ef08cc83f1617
Kelnite <kelvinmulyawan2000@gmail.com>
2025-04-02 21:31:53 +0700

    Hi !

---
+++ Helplot.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+plt.style.use("seaborn-v0_8-whitegrid")
+
+class Helplot:
+  """
+  TensorFlow Model Training History Plot
+  
+  >>> history = model.fit(train, ...)
+  >>> plot_hist = Helplot(history, 10)
+  >>> plot_hist.Relplot
+  >>> plot_hist.Falplot
+  """
+  def __init__(self, hist, loop, logits):
+    self.hist = hist
+    self.loop = loop
+
+  @property
+  def Relplot(self):
+    """
+    """
+    plt.plot(self.loop)
+    if self.val_log in self.hist:
+      plt.plot(self.loop, self.val_log)
+    plt.title("Model Result")
+    plt.legend();
+
+  @property
+  def Falplot(self):
+    """
+    Model Error Plot
+    """
+    plt.plot(self.loop)
+    if self.val_loss in self.hist:
+      plt.plot(self.loop, self.val_loss)
+    plt.title("Model Error")
+    plt.legend();
