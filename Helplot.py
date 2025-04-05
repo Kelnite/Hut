9f2055b992c5edd1d856d866b6bfbd9c02767a70
Kelnite <kelvinmulyawan2000@gmail.com>
2025-04-05 17:08:41 +0700

    Hi !

--- Helplot.py
+++ Helplot.py
@@ -7,28 +7,31 @@ class Helplot:
   TensorFlow Model Training History Plot
   
   >>> history = model.fit(train, ...)
-  >>> plot_hist = Helplot(history, 10)
+  >>> plot_hist = Helplot(history, 'accuracy')
   >>> plot_hist.Relplot
   >>> plot_hist.Falplot
   """
-  def __init__(self, hist, loop, metrics):
-    self.hist = hist
-    self.loop = loop
+  def __init__(self, hist, metrics):
+    self.hist = hist.history
+    self.loop = [*range(1, len(self.hist['loss']) + 1)]
     self.metrics = metrics
 
   def labeler(self, xlabel, ylabel, title, legend=False):
-    plt.title(title)
     plt.xlabel(xlabel); plt.ylabel(ylabel)
     if legend:
       plt.legend()
+    plt.title(title)
 
   @property
   def Relplot(self):
     """
     Model Result
     """
-    metrics = self.hist.history[self.metrics]
-    plt.plot(self.loop, metrics, label='Train Accuracy')
+    result = self.hist[self.metrics]
+    plt.plot(self.loop, result, label='Train Accuracy')
+    if 'val_' + self.metrics in self.hist.keys():
+      val_result = self.hist['val_' + self.metrics]
+      plt.plot(self.loop, val_result, label='Validation Accuracy')
     self.labeler('Epochs', 'Accuracy', 'Model Result', True)
     plt.xticks(self.loop);
 
@@ -37,7 +40,10 @@ class Helplot:
     """
     Model Error Plot
     """
-    loss = self.hist.history['loss']
+    loss = self.hist['loss']
     plt.plot(self.loop, loss, label='Train Loss')
+    if 'val_loss' in self.hist.keys():
+      val_loss = self.hist['val_loss']
+      plt.plot(self.loop, val_loss, label='Validation Loss')
     self.labeler('Epochs', 'Loss', 'Model Error', True)
     plt.xticks(self.loop);
